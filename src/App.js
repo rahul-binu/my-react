6ab6d4e2a3128b9ed8f0d1a1b7ae53a21b639d74
rahul-binu <just.commit.it@gmail.com>
2024-01-13 20:46:15 +0530

    j

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import './App.css';
-
-function Home() {
-  return (
-    <div class="Home">
-      <h3>Hello World</h3>
-    </div>
-  );
-}
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <ul>
-        <li>
-          <p >Home</p>
-        </li>
-        <li>
-          <p>Next</p>
-        </li>
-      </ul>
-    </div>
+    <Router>
+      <div>
+        <Route path="/" exact component={Home} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+//import React from React;
+
+function Home(){
+    return(
+        <div>
+            <h1>
+                Home Page
+            </h1>
+        </div>
+    );
+}
+
+export default Home;
