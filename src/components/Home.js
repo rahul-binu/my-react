3f1ef525d8bb347db800e7b004c7111c57fab9bb
rahul-binu <just.commit.it@gmail.com>
2024-01-20 19:18:02 +0530

    newbranch

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,10 @@
 import React from 'react';
-//import 'bootstrap/dist/css/bootstrap.min.css';
-//import 'bootstrap/dist/js/bootstrap.bundle.min';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import '../styles/Home.css';
 
 function Home() {
   return (
     <div>
-      <Col className="Title">Home Page</Col>
+      <h1 className="Title">Home Page</h1>
       <h4>lorem23jdfbjdbvkdnvbgdvierdfviheisdfhiuvw</h4>
       <a href="">Anchor</a>
     </div>
