4ebfe405977f3e8ab815cbe644733e6b3fc023b8
rahul-binu <just.commit.it@gmail.com>
2024-01-24 18:23:45 +0530

    new

---
+++ src/components/ConditionalRendering.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+function ConditionalRendering() {
+    let content;
+    var isLoggedIn = false;
+    if (isLoggedIn) {
+        content = <p>Welcome</p>;
+    } else {
+        content = <p> log In pleace </p>;
+    }
+    {isLoggedIn ? (<p>conditional statement <br />you are welcome</p>) :(<p>please login </p>)}
+    return (
+        <div>
+            {content}
+        </div>
+    );
+}
+
+export default ConditionalRendering;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,21 @@
 import React from 'react';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 //import 'bootstrap/dist/js/bootstrap.bundle.min';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+//import Container from 'react-bootstrap/Container';
+//import Row from 'react-bootstrap/Row';
+//import Col from 'react-bootstrap/Col';
 import '../styles/Home.css';
 
 function Home() {
   return (
     <div>
-      <Col className="Title">Home Page</Col>
+      <p className="Title">Home Page</p>
       <h4>lorem23jdfbjdbvkdnvbgdvierdfviheisdfhiuvw</h4>
       <a href="">Anchor</a>
+      <button>hai</button>
+      <br />
+      <hr />
+      <a href="">hfdoeuhfb</a>
     </div>
   );
 }
