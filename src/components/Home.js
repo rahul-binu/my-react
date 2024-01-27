f5c65ba6784b59dc8881d21b34a165ce5989cc2f
rahul-binu <just.commit.it@gmail.com>
2024-01-27 20:52:20 +0530

    K

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,4 @@
 import React from 'react';
-//import 'bootstrap/dist/css/bootstrap.min.css';
-//import 'bootstrap/dist/js/bootstrap.bundle.min';
-//import Container from 'react-bootstrap/Container';
-//import Row from 'react-bootstrap/Row';
-//import Col from 'react-bootstrap/Col';
 import '../styles/Home.css';
 
 function Home() {
